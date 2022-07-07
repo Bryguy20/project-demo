5857ca19ba313c226f4e5d6e1f6fe77a2cd43091
Ben Kawalec <bkawalec10@gmail.com>
2022-07-06 23:43:40 -0400

    Redid all JS, HTML, and CSS

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,55 @@
-const text = document.querySelector(".text-input")
-const submit = document.querySelector('.button')
-const toDoItems = document.querySelector('#to-do-items')
-const placeholder = document.querySelector('.placeholder')
-
-text.textContent = "Hello"
-let inputValue = []
-
-let data = JSON.parse(localStorage.getItem("tasks"))
-
-submit.onclick = function(event){
-    event.preventDefault();
-
-
-    inputValue.push(text.value);
+(function(){
+  
+    let list = document.querySelector('#list')
+    let form = document.querySelector('form')
+    let item = document.querySelector('#item')
+    let button = document.querySelector('#submit')
     
-// Gives error if no text
-    if(text.value.length == 0){
-        alert("Please Enter a Task")
+
+    // Toggles event when you press "Enter"
+    form.addEventListener('submit',function(event){
+      event.preventDefault();
+      list.innerHTML += '<li>' + item.value + '</li>';
+      store();
+      item.value = "";
+    },false)
+    
+    // Toggles event when you click the "Add" button
+    button.addEventListener('click', function(event){
+        event.preventDefault();
+        list.innerHTML += '<li>' + item.value + '</li>';
+        store();
+        item.value = "";
+      },false)
+
+
+    // Checks if you click on one of the todo elements  
+    list.addEventListener('click',function(event){
+      var test = event.target;
+      if(test.classList.contains('checked')){
+        test.parentNode.removeChild(test);
+      } else {
+        test.classList.add('checked');
+      }
+      store();
+    },false)
+    
+
+    // Stores todo items in local storage
+    function store() {
+      window.localStorage.myitems = list.innerHTML;
     }
-// Adds Item 
-    else{
-        toDoItems.innerHTML += 
-        `<div class="task">
-            <label class="delete1">
-                <input type="checkbox" id="contactChoice1">
-            </label>
-            <li id="taskname">${text.value}</li>
-        </div>`;
+    
 
-        text.value = ""
-        text.placeholder = "Add to do"
-
-// Deletes task
-        var current_tasks = document.querySelectorAll(".delete1");
-        for(var i=0; i < current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-        data.push(inputValue)
-        localStorage.setItem('tasks', JSON.stringify(data))
+    // Retreives the locally stored items and updates the html
+    function getValues() {
+      var storedValues = window.localStorage.myitems;
+      if(!storedValues) {
+        list.innerHTML = '';
+      }
+      else {
+        list.innerHTML = storedValues;
+      }
     }
-
-    
-    
-    
-}
-
-
-
-
-window.onload = loadTasks
-
-
-
-
-
-function loadTasks(){
-  let tasks = JSON.parse(localStorage.getItem('tasks'))
-
-  NumOfTasks = tasks.length
-
-    for (let i = 0; i < NumOfTasks; i++) {
-        toDoItems.innerHTML += 
-        `<div class="task">
-            <label class="delete1">
-                <input type="checkbox" id="contactChoice1">
-            </label>
-            <li id="taskname">${tasks[i]}</li>
-        </div>`;
-    }
-}
-
+    getValues();
+  })();
